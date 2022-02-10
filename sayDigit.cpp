efb78ab1b310b3cb306d27b83cd009d2885e7d29
Dipankar Roy <51451728+dip112@users.noreply.github.com>
2022-02-10 21:15:54 +0530

    Add files via upload

---
+++ sayDigit.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+
+void sayDigit(int n , string s[])
+{
+  if(n==0)
+    return;
+  int digit=n%10;
+  n=n/10;
+
+  sayDigit(n , s);
+
+  cout<<s[digit]<<" ";
+}
+
+int main()
+{
+  string s[10]={"zero" , "one" , "two" , "three" , "four" , "five" ,
+            "six" , "seven" , "eight" , "nine"};
+
+  int n;
+  cout<<"Enter number\n";
+  cin>>n;
+
+  sayDigit(n , s);
+
+  return 0;
+}
