3fbc1283c12725a5ffa59d004fe40479d96e456b
Dipankar Roy <51451728+dip112@users.noreply.github.com>
2022-03-01 11:47:23 +0530

    Add files via upload

---
+++ findTheWinner.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+int findTheWinner(int n, int k)
+   {
+       vector<int> v;
+
+       for(int i=1;i<=n;i++)
+       {
+           v.push_back(i);
+       }
+
+       int i=0;
+       while(v.size()>1)
+       {
+           int n=v.size();
+           i=(i+k-1)%n;
+           v.erase(v.begin()+i);
+       }
+       int res=0;
+       if(v.size()==1)
+       {
+         res=v[0];
+       }
+       return res;
+   }
+int main()
+{
+  int n , k;
+  cout<<"ENter the value of n and k\n";
+  cin>>n>>k;
+
+  cout<<"Result: "<<findTheWinner(n , k)<<endl;
+  return 0;
+}
