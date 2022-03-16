11c42cf25b674a4a0dd8c101b10e1f7ee1099c0e
Dipankar Roy <51451728+dip112@users.noreply.github.com>
2022-03-16 09:57:51 +0530

    Create validStackSequence.cpp

---
+++ validStackSequence.cpp
@@ -0,0 +1,22 @@
+//https://leetcode.com/problems/validate-stack-sequences/
+class Solution {
+public:
+    bool validateStackSequences(vector<int>& pushed, vector<int>& popped) 
+    {
+        stack<int>s;
+        
+        int j=0;
+        
+        for(int i=0;i<pushed.size();i++)
+        {
+            s.push(pushed[i]);
+            
+            while(!s.empty() && s.top()==popped[j])
+            {
+                s.pop();
+                j++;
+            }
+        }
+        return s.empty();
+    }
+};
