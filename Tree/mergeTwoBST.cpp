de83a1aebcaa22bf44ece26ce90a9e50122195fb
Dipankar Roy <51451728+dip112@users.noreply.github.com>
2022-07-13 10:06:56 +0530

    Update mergeTwoBST.cpp

--- Tree/mergeTwoBST.cpp
+++ Tree/mergeTwoBST.cpp
@@ -48,3 +48,99 @@ TreeNode<int> *mergeBST(TreeNode<int> *root1, TreeNode<int> *root2){
 }
 //Time: O(m+n)
 //Space: O(m+n)
+
+//approach2
+void BSTtoSortedLL(TreeNode<int> *root , TreeNode<int> *&head){
+    if(root==NULL){
+        return ;
+    }
+    BSTtoSortedLL(root->right , head);
+    root->right=head;
+    if(head!=NULL){
+        head->left=root;
+    }
+    head=root;
+    BSTtoSortedLL(root->left , head);
+}
+TreeNode<int> *mergeSortedLL(TreeNode<int> *&head1 , TreeNode<int> *&head2){
+    TreeNode<int> *head=NULL;
+    TreeNode<int> *tail=NULL;
+    while(head1!=NULL && head2!=NULL){
+        if(head1->data<head2->data){
+            if(head==NULL){
+                head=head1;
+                tail=head1;
+                head1=head1->right;
+            }
+            else{
+                tail->right=head1;
+                head1->left=tail;
+                tail=head1;
+                head1=head1->right;
+            }
+        }
+        else{
+            if(head==NULL){
+                head=head2;
+                tail=head2;
+                head2=head2->right;
+            }
+            else{
+                tail->right=head2;
+                head2->left=tail;
+                tail=head2;
+                head2=head2->right;
+            }
+        }
+    }
+    while(head1!=NULL){
+        tail->right=head1;
+        head1->left=tail;
+        tail=head1;
+        head1=head1->right;
+    }
+    while(head2!=NULL){
+        tail->right=head2;
+        head2->left=tail;
+        tail=head2;
+        head2=head2->right;
+    }
+    return head;
+}
+int countNodes(TreeNode<int> *head){
+    TreeNode<int>*temp=head;
+    int count=0;
+    while(temp!=NULL){
+        count++;
+        temp=temp->right;
+    }
+    return count;
+}
+TreeNode<int>* sortedLLtoBST(TreeNode<int>* &head , int n){
+    if(n<=0 || head==NULL){
+        return NULL;
+    }
+    TreeNode<int>*left=sortedLLtoBST(head , n/2);
+    TreeNode<int>*root=head;
+    root->left=left;
+    head=head->right;
+    root->right=sortedLLtoBST(head , (n-n/2-1));
+    return root;
+}
+TreeNode<int> *mergeBST(TreeNode<int> *root1, TreeNode<int> *root2){
+    //convert 1st
+    TreeNode<int> *head1=NULL;
+    BSTtoSortedLL(root1 , head1);
+    head1->left=NULL;
+    
+    TreeNode<int> *head2=NULL;
+    BSTtoSortedLL(root2 , head2);
+    head2->left=NULL;
+    
+    TreeNode<int> *head=mergeSortedLL(head1 , head2);
+    
+    int n=countNodes(head);
+    return sortedLLtoBST(head , n);
+}
+//Time: O(M+N)
+//Spcce: O(H1+H2)
