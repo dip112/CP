98e109978f4db0601c0342b82093d1383650a0dc
Dipankar Roy <51451728+dip112@users.noreply.github.com>
2022-07-24 18:25:33 +0530

    Update nQueens.cpp

--- Backtraking/nQueens.cpp
+++ Backtraking/nQueens.cpp
@@ -63,5 +63,77 @@ vector<vector<int>> nQueens(int n)
     
 	return ans;
 }
+
+//leetcode
+class Solution {
+    void addAns(int n , vector<vector<char>>&board , vector<vector<string>>&ans){
+        vector<string>temp;
+        string s="";
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                s+=board[i][j];
+            }
+            temp.push_back(s);
+            s.clear();
+        }
+        ans.push_back(temp);
+    }
+    bool isSafe(int row , int col , vector<vector<char>>&board , int n){
+        int x=row;
+        int y=col;
+        
+        while(y>=0){
+            if(board[x][y]=='Q'){
+                return 0;
+            }
+            y--;
+        }
+        
+        x=row;
+        y=col;
+
+        while(x>=0 && y>=0){
+            if(board[x][y]=='Q'){
+                return 0;
+            }
+            x--;
+            y--;
+        }
+        
+        x=row;
+        y=col;
+        
+        while(x<n && y>=0){
+            if(board[x][y]=='Q'){
+                return 0;
+            }
+            x++;
+            y--;
+        }
+        return 1;
+    }
+    void solve(int col , int n , vector<vector<char>>&board , vector<vector<string>>&ans){
+        if(col==n){
+            addAns(n , board , ans);
+            return;
+        }
+        for(int row=0;row<n;row++){
+            if(isSafe(row , col , board , n)){
+                board[row][col]='Q';
+                solve(col+1 , n , board , ans);
+                board[row][col]='.';
+            }
+        }
+    }
+public:
+    vector<vector<string>> solveNQueens(int n) {
+        vector<vector<char>>board(n , vector<char>(n,'.'));
+        vector<vector<string>>ans;
+        
+        solve(0 , n , board , ans);
+        return ans;
+    }
+};
+
 //Time: O(n!)
 //Space: O(n*n)
