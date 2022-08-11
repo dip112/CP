6b371dac848acf0e3feffd3a9a4521a191ed2d31
Dipankar Roy <51451728+dip112@users.noreply.github.com>
2022-08-11 11:54:23 +0530

    Update cycleDetectionInDirected.cpp

--- Graph/cycleDetectionInDirected.cpp
+++ Graph/cycleDetectionInDirected.cpp
@@ -1,5 +1,7 @@
 //https://practice.geeksforgeeks.org/problems/detect-cycle-in-a-directed-graph/1
 //https://www.codingninjas.com/codestudio/problems/detect-cycle-in-a-directed-graph_1062626?leftPanelTab=0&utm_source=youtube&utm_medium=affiliate&utm_campaign=Lovebabbar
+
+//using dfs
 #include<bits/stdc++.h>
 bool cycleDetectDFS(int node , unordered_map<int , bool>&visited , unordered_map<int , bool>&dfsVisited ,                              unordered_map<int , list<int>>&adj){
     visited[node]=1;
@@ -41,4 +43,53 @@ int detectCycleInDirectedGraph(int n, vector < pair < int, int >> & edges) {
         }
     }
     return false;
+}
+
+//using bfs|kahn's algorithm
+#include<bits/stdc++.h>
+int detectCycleInDirectedGraph(int v, vector < pair < int, int >> & edges) {
+  unordered_map<int , list<int>>adj;
+    //create adjacency list
+    for(int i=0;i<edges.size();i++){
+        int u=edges[i].first-1;    //as in this case node start from 1, to make it 0 subtract by 1
+        int v=edges[i].second-1;
+        adj[u].push_back(v);
+    }
+    
+    //find indigree of all nodes
+    vector<int>indegree(v);
+    for(auto i:adj){
+        for(auto j:i.second){
+            indegree[j]++;
+        }
+    }
+    
+    //push nodes with 0 indegree
+    queue<int>q;
+    for(int i=0;i<v;i++){
+        if(indegree[i]==0){
+            q.push(i);
+        }
+    }
+    
+    int cnt=0;
+    //do bfs
+    while(!q.empty()){
+        int front=q.front();
+        q.pop();
+        cnt++;
+        //find neighbours
+        for(auto neighbour:adj[front]){
+            indegree[neighbour]--;
+            if(indegree[neighbour]==0){
+                q.push(neighbour);
+            }
+        }
+    }
+    if(cnt==v){    //for valid topological sort counts equal to number of nodes
+        return false;
+    }
+    else{
+        return true;
+    }
 }
